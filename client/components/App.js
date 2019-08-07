bdd98c1c2465f2e02681c810d85f5a9367cef76d
Brooke <brooke.snellings@gmail.com>
2019-08-07 12:39:27 -0500

    Add Header component

--- client/components/App.js
+++ client/components/App.js
@@ -7,7 +7,7 @@ class App extends React.Component {
   }
 
   render() {
-    return <h1>Museumly</h1>;
+    return <h1>Museumly Browse Search Favorites</h1>;
   }
 }
 
---
+++ client/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from './Header.js';
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default Header;
