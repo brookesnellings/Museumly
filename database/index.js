aee289efbeada56f269637d5d63492df70865c4b
Brooke <brooke.snellings@gmail.com>
2019-08-07 12:49:27 -0500

    Add MySQL

--- database/index.js
+++ database/index.js
@@ -1,1 +1,15 @@
-// Filler file for database
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'museumly'
+});
+
+connection.connect(err => {
+  if (err) {
+    console.log('db not connected: ', err);
+  } else {
+    console.log('db connected!');
+  }
+});
---
+++ database/schema.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS museumly;
+CREATE DATABASE museumly;
+
+USE museumly;
+
+CREATE TABLE artworks (
+  artwork_id BIGINT PRIMARY KEY,
+  artist VARCHAR(255),
+  title VARCHAR(255),
+  image VARCHAR(255)
+);
