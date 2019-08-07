be3484a3502c87f93fb7e766939dbc6031e86140
Brooke <brooke.snellings@gmail.com>
2019-08-07 13:45:41 -0500

    Add Collections & Favorites to Navbar

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navbar, Form, Button } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
 
 class Header extends React.Component {
   constructor(props) {
@@ -13,7 +13,23 @@ class Header extends React.Component {
         <Navbar bg="light" expand="lg">
           <Navbar.Brand href="#home">Museumly</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav"></Navbar.Collapse>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#collections">Collections</Nav.Link>
+              <Nav.Link href="#favorites">Favorites</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Form inline>
+              <FormControl type="text" placeholder="Search by Artist" className="mr-sm-2" />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
         </Navbar>
       </div>
     );
