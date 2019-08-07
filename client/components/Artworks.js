65f37db54cc371b684e0d5c45e47d9b98256822d
Brooke <brooke.snellings@gmail.com>
2019-08-07 12:41:15 -0500

    Add Artworks component

---
+++ client/components/Artworks.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Artworks extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div> artwork images will go here</div>;
+  }
+}
+
+export default Artworks;
