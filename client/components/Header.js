93cdb4e2bd202303a8600118b60927f04d126452
Brooke <brooke.snellings@gmail.com>
2019-08-08 16:08:28 -0500

    Edit header

--- client/components/Header.js
+++ client/components/Header.js
@@ -28,9 +28,9 @@ function Header(props) {
             >
               Favorites
             </Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+            <NavDropdown title="Explore By" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Artist</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Collection</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
@@ -49,6 +49,8 @@ function Header(props) {
               onClick={() => {
                 console.log('clickety clicked');
                 props.searchForArtist(props.userInput);
+                props.showSearched(true);
+                props.showFavorites(false);
               }}
             >
               Search
