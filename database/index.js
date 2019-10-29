53ddb4cac3da68685896f648912561fdf6207806
Brooke <brooke.snellings@gmail.com>
2019-10-28 21:41:08 -0500

    Deploy DB

--- database/index.js
+++ database/index.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 const config = require('../config.js');
 
-const connection = mysql.createConnection(config.DBCONFIG);
+const connection = mysql.createConnection(process.env.DATABASE_URL);
 
 connection.connect(err => {
   if (err) {
